eab7d2fb71230e46cbd99a43b5c0323eb3e534a2
Gheorghita Stefana <gheorghitastefana@gmail.com>
2023-10-25 18:15:02 +0300

    Update ex6.py

--- Lab3/ex6.py
+++ Lab3/ex6.py
@@ -5,6 +5,18 @@ def find_tuple(ls):
     return a, b
 
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 2, 2]
-print(find_tuple(list))
+def find_tuple2(ls):
+    unique = set()
+    duplicate = set()
+    for el in ls:
+        count = ls.count(el)
+        if count == 1:
+            unique.add(el)
+        else:
+            duplicate.add(el)
+    return len(unique), len(duplicate)
 
+
+list = [1, 2, 2, 2, 3, 4, 4, 4, 5, 6, 7, 7]
+print(find_tuple(list))
+print(find_tuple2(list))
